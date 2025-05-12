08cd4eef423e7c4a241245686376e3c225c384bc
Trịnh Lê Huy <huytrinh.22032004@gmail.com>
2025-05-12 17:22:02 +0700

    Update Page

--- Front-end/src/routes/index.js
+++ Front-end/src/routes/index.js
@@ -3,13 +3,11 @@ import { SignupLayout } from '~/components/Layout';
 import Login from '~/pages/Login';
 import Home from '../pages/Home';
 import Signup from '~/pages/Signup';
-import Menu from '../pages/Menu';
 import Payment from '../pages/Payment';
 
 // Public routes
 const publicRoutes = [
     { path: '/', component: Home },
-    { path: '/menu', component: Menu },
     { path: '/payment', component: Payment },
     { path: '/login', component: Login, layout: LoginLayout },
     { path: '/signup', component: Signup, layout: SignupLayout },
@@ -17,4 +15,4 @@ const publicRoutes = [
 
 const privateRoutes = [];
 
-export { privateRoutes, publicRoutes }; 
+export { privateRoutes, publicRoutes };
